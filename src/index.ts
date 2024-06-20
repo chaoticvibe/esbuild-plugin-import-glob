e0bfe62d8abb3ee9ec638a935bc69be3cbf120ec
Shelby <pedrohenriqueissa@gmail.com>
2024-06-20 05:46:58 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import fastGlob from 'fast-glob';
 import { Plugin } from 'esbuild';
-
-const EsbuildPluginImportGlob = (): Plugin => ({
+import fs from 'fs';
+import path from 'node:path';
+import Ajv from 'ajv/dist/2019';
+import addFormats from 'ajv-formats';
+import { default as standaloneCode } from 'ajv/dist/standalone';
+const EsbuildPluginImportGlob = (options): Plugin => ({
   name: 'require-context',
   setup: (build) => {
     build.onResolve({ filter: /\*/ }, async (args) => {
@@ -24,22 +28,90 @@ const EsbuildPluginImportGlob = (): Plugin => ({
           cwd: args.pluginData.resolveDir,
         })
       ).sort();
+   
+      const nonShemaFiles = files.filter((file) => !file.endsWith('schema.json'));
+      const schemaFiles = files.filter((file) => file.endsWith('schema.json'));
+      const compileSchema = (filePath) => {
+        const file = fs.readFileSync(filePath, 'utf-8');
+        const defaultAjvOptions = { ...options };
+        // Maybe will be used in future
+        // { sourceCode: true } should not be overridden
+        if (options.serverSide && options.ajv && options.ajv.allErrors) {
+          options.ajv.allErrors = false;
+        }
+
+        if (options.serverSide) {
+          defaultAjvOptions.allErrors = false;
+        }
+
+        const ajvOptions = Object.assign(
+          {},
+          defaultAjvOptions,
+          options.ajv || {},
+          {
+            code: { source: true },
+          }
+        );
+
+        let ajv = new Ajv(ajvOptions);
+        if (!options.serverSide) {
+          require('ajv-errors')(ajv /*, {singleError: true} */);
+        }
+        addFormats(ajv);
+        let schema;
+
+        try {
+          schema = JSON.parse(file);
+        } catch (e) {
+          console.log('Schema is not a valid JSON: ' + filePath);
+          return;
+        }
+        const validate = ajv.compile(schema);
+        if (validate) {
+          // 1. generate module with a single default export (CommonJS and ESM compatible):
+          return standaloneCode(ajv, validate).toString();
+        } else {
+          console.log({ message: 'Fail to compile schema', filePath });
+          return null;
+        }
+      };
+      const schemas = {};
       
-      console.log(args.path);   
+      schemaFiles.forEach((
+        file
+      )=>{
+        const schema = compileSchema(file);
+        if(schema){
+          schemas[file] = schema;
+        }
+       
+      });
+      const schemaCompiledFiles = Object.keys(schemas);
+  
 
       let importerCode = `
-        ${files
-          .map((module, index) => `import * as module${index} from '${module}'`)
+      ${nonShemaFiles
+        .map((module, index) => `import * as module${index} from '${module}'`)
+        .join(';')}
+        
+        ${schemaCompiledFiles
+          .map((module, index) => `const schema${index} = ${schemas[module]}`)
           .join(';')}
-
-        const modules = [${files
+    
+      let fileModules = [${nonShemaFiles
           .map((module, index) => `module${index}`)
           .join(',')}];
+        
+      const modules = fileModules.concat([${schemaCompiledFiles
+          .map((module, index) => `schema${index}`)
+          .join(',')}]);
 
         export default modules;
-        export const filenames = [${files
+        export const filenames = [...[${nonShemaFiles
           .map((module, index) => `'${module}'`)
-          .join(',')}]
+          .join(',')}], [${schemaCompiledFiles
+            .map((module, index) => `'${module}'`)
+            .join(',')}]]
       `;
 
       return { contents: importerCode, resolveDir: args.pluginData.resolveDir };
